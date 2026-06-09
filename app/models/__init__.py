e535d6ebf5c85c6d8760647df55c9fa8dc40da3f
Raju Mannem <rajumannem71@gmail.com>
2026-06-10 02:36:03 +0530

    feat: export core models in app/models package initialization

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
-from .merchant import Base, Merchant
+from .merchant import Merchant
 from .bill import Bill
 from .enums import BillStatus
+from .chat import ChatMessage
 
-__all__ = ["Base", "Merchant", "Bill", "BillStatus"]
+__all__ = ["Merchant", "Bill", "BillStatus", "ChatMessage"]
